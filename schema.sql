ed4010f36cd3bf472d9ec574393523972b7dcb80
Robert Wang <robwang.us@gmail.com>
2024-02-07 22:08:47 -0600

    update requirements

--- schema.sql
+++ schema.sql
@@ -1,12 +1,9 @@
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    added_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    username VARCHAR(50),
-    password VARCHAR(50),
+CREATE TABLE user_account (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    password_hash VARCHAR(255)
 );
-
 CREATE TABLE houses (
     house_id SERIAL PRIMARY KEY,
     house_name VARCHAR(50) NOT NULL,
