645adbefff7756724b28caac066b801a961d698d
Robert Wang <robwang.us@gmail.com>
2024-02-07 21:57:38 -0600

    Created database, user login, account creation, password hashing

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 # kluver might want us to use psycopg2 instead
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
+from data import *
 
 
 # static_url_path allows us to link js files without needing "../" in front
@@ -9,15 +11,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://something'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'N^85r7b85rF&$%VR754e6cv43WC&*)&h(^42WNI69&6'  
 
+bcrypt = Bcrypt(app)
+
+with app.app_context():  # Manually push an application context
+    setup()  # Now you can call setup() and current_app will be available
+
 # login page
-@app.route('/')
-def login():
+@app.route('/', methods=['GET', 'POST'])
+def login():    
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user_record = check_user_exists(username, password)
+        if user_record:
+            is_valid = bcrypt.check_password_hash(user_record[0], password)
+            if is_valid: 
+                return redirect("/user/home")
+            else:
+                flash("Incorrect username or password")
+                return render_template('login.html')
+        else:
+            flash("Incorrect username or password")
+            return render_template('login.html')
+    
     return render_template('login.html')
 
 # create new account page
 @app.route('/create/acct', methods=['GET', 'POST'])
 def create_acct():
     if request.method == 'POST':
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        add_user_account(username, email, password_hash)
         return redirect("/user/home")
     else:
         return render_template('create_acct.html')
