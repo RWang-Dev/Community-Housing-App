78fe0853d5d9c6ce50f8e7740467de501b9868f9
Justin Lee <justinlee01656@gmail.com>
2024-02-07 20:58:32 -0600

    created password match check

--- static/js/create_acct.js
+++ static/js/create_acct.js
@@ -1,1 +1,14 @@
-/*Create function that checks if passwords match*/
+function validatePasswords() {
+    const passwordInput = document.getElementById("password");
+    const confirmPasswordInput = document.getElementById("confirm_password");
+    const passwordError = document.getElementById("password-error");
+  
+    if (passwordInput.value !== confirmPasswordInput.value) {
+      passwordError.textContent = "Passwords do not match";
+      return false;
+    } else {
+      passwordError.textContent = "";
+      return true;
+    }
+  }
+  
