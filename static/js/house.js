868823b43e0e3e0d7720bf9d642ae5467d2df9df
Stephen <ma000094@umn.edu>
2024-02-17 16:32:14 -0600

    need help w adding tasks to calendar in house.html: added get_tasks_by_house_id in data.py, get_tasks route in server.py, minor changes to house.js and house.html

--- static/js/house.js
+++ static/js/house.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth', 
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay' // Buttons for monthly, weekly, and daily views
-      }
-    });
-    calendar.render();
+  var calendarEl = document.getElementById('calendar');
+  var houseId = calendarEl.dataset.houseId;
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth', 
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay' // Buttons for monthly, weekly, and daily views
+    },
+    events: `/get-tasks/${houseId}`
+  });
+  calendar.render();
 });
